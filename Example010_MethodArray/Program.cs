7692214a135a940df4e2e1205b6f4aa6822ca1f3
DeathBlamer <deathblamer@gmail.com>
2023-06-22 11:47:02 +0500

    Add Example010 and Example 011

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 12, 213, 23, 54, 546, 602, 12, 98 };
+
+int n = array.Length;
+int find = 12;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
